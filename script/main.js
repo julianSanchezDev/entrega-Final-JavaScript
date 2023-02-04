2df8311215b0d3b77b52bf9d2f9124d1511589f2
BetaTester95 <97420300+BetaTester95@users.noreply.github.com>
2023-02-04 18:31:39 -0300

    actualizacion

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+
+
 async function pokeFor(number) {
     let pokemones = [];
     for (let i = 1; i <= number; i++) {
@@ -12,8 +14,7 @@ async function pokeFor(number) {
 pokeFor(24);
 
 function renderizarPokemons(pokemones) {
-    let container = document.getElementById("resultado");
-    let salida = "";
+   
     for (let i = 0; i < pokemones.length; i++) {
 
         let columna = document.createElement("div");
@@ -34,27 +35,65 @@ function renderizarPokemons(pokemones) {
 
         let div_hijo2 = document.createElement("div");
         div_hijo2.className = "card-body";
-
-        let aHijo= document.createElement("a");
-        aHijo.className= "btn btn-primary";
-        aHijo.textContent="Capturar Pokemon";
-        
         
         let parrafo = document.createElement("p");
         
-
         div_hijo1.innerHTML = `<h4> ${pokemones[i].name} </h4>`;
         div_hijo2.appendChild(parrafo);
         div_padre.appendChild(div_hijo1);
         div_hijo2.appendChild(imagen);
         div_padre.appendChild(div_hijo2);
-        div_hijo2.appendChild(aHijo);
         columna.appendChild(div_padre);
         resultado.appendChild(columna);
         
      
 }
 }
+
+
+
+function mostrar() {
+    let input1 = document.getElementById("input1").value;
+    let inputPass = document.getElementById("inputPass").value;
+
+    if (input1 === "") {
+
+        Swal.fire({
+            title: 'Error',
+            text: 'Por favor complete el formulario',
+            icon: 'error'
+          });
+      
+    }
+
+    else{
+
+        Swal.fire(
+            'Enviado!',
+            'Sus datos han sido registrados!',
+            'success'
+          )
+
+    }
+  
+    let arrayInput = JSON.parse(localStorage.getItem("inputArray")) || [];
+    if (arrayInput.indexOf(input1) === -1 ) {
+        arrayInput.push(input1);
+      }
+      localStorage.setItem("inputArray", JSON.stringify(arrayInput));
+
+    let arrayPass = JSON.parse(localStorage.getItem("arrayPass")) || [];
+    if (arrayPass.indexOf(inputPass) === -1 ) {
+        arrayPass.push(inputPass);
+      }
+      localStorage.setItem("arrayPass", JSON.stringify(arrayPass));
+
+
+  }
+  
+  let boton1 = document.getElementById("boton1");
+  boton1.addEventListener("click", mostrar);
+
 
 
 
